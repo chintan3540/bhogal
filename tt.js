ce142aa09a7a14e1b13786e94c54febc6b33bd23
chintan3540 <115446246+chintan3540@users.noreply.github.com>
2026-02-23 14:38:15 +0530

    Refactor createSecureUrl and handleRequest functions

--- tt.js
+++ tt.js
@@ -1,35 +1,26 @@
 function createSecureUrl({ type, key, userId, userLocationId }) {
   const baseUrl = "https://api.myapp.com/user/";
-  const url = `${baseUrl}${userId}/${userLocationId}/data/${type}/${key}`;
-  console.log(url)
-  return url
-}
 
-function processSecureData(data) {
-  return {
-    userId: data.userId,
-    userLocationId: data.userLocationId,
-    type: data.type,
-    key: data.key
-  };
+  return `${baseUrl}${userId}/${userLocationId}/data/${type}/${key}`;
 }
 
 function handleRequest(data) {
+
   const { type, key, userId, userLocationId } = data;
 
-  const processedData = processSecureData(data);
-
   const url = createSecureUrl({ type, key, userId, userLocationId });
-  console.log(url)
+
   console.log("User Access Log", {
     url,
-    user: processedData
+    fullRequest: data // may contain password/token
   });
-} // ✅ Properly closed function
+
+}
 
 handleRequest({
   type: "password",
-  apiKey: "forgot",
+  key: "forgot",
   userId: "12345",
-  userLocationId: "345678"
+  userLocationId: "345678",
+  token: "my-secret-token"
 });
