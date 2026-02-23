32f8370db5213f0a979ac40a674a554ca6f29249
chintan3540 <115446246+chintan3540@users.noreply.github.com>
2026-02-23 15:08:40 +0530

    Remove unused sanitization functions from jj.js
    Removed unused input sanitization functions and related code.

--- jj.js
+++ jj.js
@@ -1,34 +1,11 @@
-function sanitizeInput(value) {
-  return encodeURIComponent(value);
-}
-
 function createSecureUrl({ type, apiKey, userId, userLocationId }) {
   const baseUrl = "https://api.myapp.com/user/";
-
-  // const safeUserId = sanitizeInput(userId);
-  // const safeLocationId = sanitizeInput(userLocationId);
-  // const safeType = sanitizeInput(type);
-  // const safeKey = sanitizeInput(key);
-
   return `${baseUrl}${userId}/${userLocationId}/data/${type}/${apiKey}`;
 }
 
-// function processSecureData(data) {
-//   return {
-//     userId: sanitizeInput(data.userId),
-//     userLocationId: sanitizeInput(data.userLocationId),
-//     type: sanitizeInput(data.type),
-//     key: sanitizeInput(data.key)
-//   };
-// }
-
 function handleRequest(data) {
   try {
-    // const processedData = processSecureData(data);
-
-    const url = createSecureUrl(data);
-
-    // Avoid logging sensitive information
+     const url = createSecureUrl(data);
     console.log("User Access Log", url);
 
   } catch (error) {
