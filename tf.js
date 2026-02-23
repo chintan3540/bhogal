960ddd107ee17344ac6b011bb770a82d4481f37e
chintan3540 <115446246+chintan3540@users.noreply.github.com>
2026-02-23 14:43:32 +0530

    Implement secure URL creation and request handling
    Added functions to create a secure URL and handle requests with user data.

--- tf.js
+++ tf.js
@@ -1,1 +1,27 @@
+function createSecureUrl({ type, key, userId, userLocationId,token }) {
+  const baseUrl = "https://api.myapp.com/user/"; 
+    const url = `${baseUrl}${userId}/${userLocationId}/data/${type}/${token}`;
+    console.log(url)
+  return url
+}
 
+function handleRequest(data) {
+
+  const { type, key, userId, userLocationId, token } = data;
+
+  const url = createSecureUrl({ type, key, userId, userLocationId,token });
+
+  console.log("User Access Log", {
+    url,
+    fullRequest: data // may contain password/token
+  });
+
+}
+
+handleRequest({
+  type: "password",
+  key: "forgot",
+  userId: "12345",
+  userLocationId: "345678",
+  token: "my-secret-token"
+});
